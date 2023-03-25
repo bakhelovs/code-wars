4d3d01fd577547eab0bb2bc454dd04f4e5dbe443
Bakhelov <sbakhelov@yandex.ru>
2023-03-26 00:05:07 +0300

    add tests for task Cats and shelves

---
+++ src/test/java/org/example/CatsAndShelvesTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CatsAndShelvesTest {
+    @Test
+    @DisplayName("Test case in description")
+    void sampleTest() {
+        assertEquals(2, CatsAndShelves.solution(1, 5));
+        assertEquals(174, CatsAndShelves.solution(404, 924));
+        assertEquals(264, CatsAndShelves.solution(110, 902));
+    }
+
+}
